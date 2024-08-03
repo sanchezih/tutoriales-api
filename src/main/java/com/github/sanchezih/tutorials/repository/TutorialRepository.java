5ac520d2db7cd46827a88d96f77632391e65a684
Ignacio Sanchez <sanchezih@gmail.com>
2024-08-03 17:09:11 -0300

    initial commit

---
+++ src/main/java/com/github/sanchezih/tutorials/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.github.sanchezih.tutorials.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.github.sanchezih.tutorials.entity.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+
+	List<Tutorial> findByTitleContaining(String title);
+}
---
+++ src/test/java/com/github/sanchezih/libros/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sanchezih.libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
